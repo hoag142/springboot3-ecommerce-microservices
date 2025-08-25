8fca4437028be40c20029a6d319e5275c40140f0
Huy Hoang <hoanghuy140204@gmail.com>
2025-08-25 22:54:21 +0700

    feat: product service

---
+++ product-service/src/main/java/com/hoag/microservices/product/service/ProductService.java
@@ -0,0 +1,39 @@
+package com.hoag.microservices.product.service;
+
+
+import com.hoag.microservices.product.dto.ProductRequest;
+import com.hoag.microservices.product.dto.ProductResponse;
+import com.hoag.microservices.product.model.Product;
+import com.hoag.microservices.product.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public ProductResponse createProduct(ProductRequest productRequest) {
+        Product product = Product.builder()
+                .name(productRequest.name())
+                .description(productRequest.description())
+                .price(productRequest.price())
+                .build();
+        productRepository.save(product);
+        log.info("Product created: " + product);
+        return new ProductResponse(product.getId(), product.getName(),product.getDescription(), product.getPrice());
+    }
+
+    public List<ProductResponse> getAllProducts() {
+        return productRepository.findAll()
+                .stream()
+                .map(product -> new ProductResponse(product.getId(), product.getName(),product.getDescription(), product.getPrice()))
+                .toList();
+    }
+}
