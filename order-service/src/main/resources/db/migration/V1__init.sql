0f3dcdbbfd0d2822a0f626eb0c8ba17d5f6b5b81
Huy Hoang <hoanghuy140204@gmail.com>
2025-08-27 02:36:48 +0700

    feat<order-service>: add flyway and controller

---
+++ order-service/src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `t_orders`
+(
+    `id`          BIGINT(20) NOT NULL AUTO_INCREMENT,
+    `order_number` varchar(255) DEFAULT NULL,
+    `sku_code` varchar(255),
+    `price` DECIMAL(19, 2),
+    `quantity` INT(11),
+    PRIMARY KEY (`id`)
+);
